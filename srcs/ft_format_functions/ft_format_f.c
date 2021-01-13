f23ae487ff44a270dad746faf4c0607a8f6cb5e2
ernestz4 <user@MBP-de-User.home>
2021-01-13 19:58:02 +0100

    format f corrige

--- srcs/ft_format_functions/ft_format_f.c
+++ srcs/ft_format_functions/ft_format_f.c
@@ -4,15 +4,16 @@ t_string	ft_format_size_f(long double nb, char *flags)
 {
 	t_string	ret;
 	int			prec;
-	int			width;
+	int			sign;
 
 	prec = 0;
-	width = 0;
 	prec = ft_strchr(flags, '.') ? ft_atoi(ft_strchr(flags, '.') + 1) + 2 : 8;
+	sign = nb < 0 ? -1 : 1;
+	nb *= sign;
 	while ((nb /= 10) > 9)
 		prec++;
 	prec++;
-	if (ft_strchr(flags, '+') || ft_strchr(flags, ' '))
+	if (ft_strchr(flags, '+') || ft_strchr(flags, ' ') || sign == -1)
 		prec++;
 	while (*flags)
 		ret.size = ret.size < ft_atoi(flags) ? ft_atoi(flags++) : ret.size;
