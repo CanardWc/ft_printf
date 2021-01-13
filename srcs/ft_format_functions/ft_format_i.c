25a8bab2b21296ad7d07cfc1667f3b8ee28f71bc
user42 <maximerochet71@gmail.com>
2021-01-13 15:59:57 +0100

    size format i et d

--- srcs/ft_format_functions/ft_format_i.c
+++ srcs/ft_format_functions/ft_format_i.c
@@ -1,5 +1,35 @@
 #include <libftprintf.h>
 
+t_string 	ft_format_size(int i, flags)
+{
+	t_string	ret;
+	char		*tmp;
+	int		nb;
+
+	nb = i;	
+	ret.size = 0;
+	tmp = flags;
+	if(nb < 0)
+	{
+		ret.size++;
+		nb *= -1;	
+	}
+	while ((nb /= 10) > 0)
+		ret.size++;
+	while (*flags)
+	{
+		ret.size = ret.size < ft_atoi(flags) ? atoi(flags) : ret.size;
+		flags++;
+	}
+	flags = tmp;
+	ret.size = i < 0 && atoi(strchr(flags, '.')) == ret.size ? ret.size++ : \
+	ret.size;
+	
+	if(!(ret.str = ft_calloc(ret.size, sizeof(char)))
+		return(0);
+	return (ret);
+}
+
 t_string	ft_format_i(va_list ap, char *flags)
 {
 	t_string	ret;
@@ -26,3 +56,18 @@ t_string	ft_format_i(va_list ap, char *flags)
 		ret.str[--size] = '0';
 	return (ret);
 }
+
+/*t_string 	ft_format_size(int i, flags)
+{
+	t_string	ret;
+
+	ret.size = ft_strlen(ft_itoa(i));
+	ret.size = ret.size < ft_atoi(ft_strchr(flags, '.') + 1) ? \
+		ft_atoi(strchr(flag, '.') : ret.size;
+	ret.size = ret.size < ft_atoi(ft_strchr(flags, '-') + 1) ? \
+		ft_atoi(strchr(flag, '-') + 1) : ret.size;
+	
+	if(!(ret.str = ft_calloc(ret.size, sizeof(char)))
+		return(0);
+	return (ret);
+}*/
