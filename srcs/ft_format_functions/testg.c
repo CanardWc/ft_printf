38972f2b3f32db16e9c5e87f002e114a5ebcfcea
Ernest Dassesse <dassesse.ernestoutlook.com>
2021-01-27 17:45:36 +0100

    still not finished

--- srcs/ft_format_functions/testg.c
+++ srcs/ft_format_functions/testg.c
@@ -6,7 +6,7 @@
 /*   By: edassess <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 16:46:41 by edassess          #+#    #+#             */
-/*   Updated: 2021/01/26 12:20:30 by edassess         ###   ########lyon.fr   */
+/*   Updated: 2021/01/27 17:45:13 by edassess         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,7 +107,7 @@ int		ft_neg_pow_f(double nb, int pow, int sign, int t_prec)
 			prec--;
 			nb /= 10.0;
 		}
-	printf("prec == %d\n", prec);
+		printf("prec == %d\n", prec);
 		return (prec);
 	}
 	nb /= 10.0;
@@ -120,36 +120,7 @@ int		ft_neg_pow_f(double nb, int pow, int sign, int t_prec)
 	printf("prec == %d\n", prec);
 	return (prec);
 }
-/*
-int		ft_size_g_f(char *flags, double nb, int pow, int sign)
-{
-	int		ret;
-	int		prec;
-	char	*t_flags;
 
-	ret = 0;
-	prec = nb > 9.0 ? 1 : 0;
-	while ((nb /= 10.0) > 9.0)
-		prec++;
-	if (strchr(flags, '.'))
-		prec = prec < atoi(strchr(flags, '.') +1) ? (double)atoi(strchr(flags, '.') +1) : prec;
-	t_flags = flags;
-	printf("prec == %d\n", prec);
-	while (*flags && *flags != '.')
-		ret = ret < atoi(flags++) ? atoi(flags - 1) : ret;
-	printf("prec == %d\n", prec);
-	if (pow < 0)
-	{
-		prec += pow * -1;
-	}
-	printf("prec == %d\n", prec);
-	printf("pow == %d\n", pow);
-	if (strchr(t_flags, ' ') || strchr(t_flags, '+') || sign == -1)
-		prec++;
-	ret = prec < ret ? ret + 1 : prec + 1;
-	return (ret);
-}
-*/
 int		ft_size_g_f(char *flags, double nb, int pow, int sign)
 {
 	int		ret;
@@ -172,23 +143,81 @@ int		ft_size_g_f(char *flags, double nb, int pow, int sign)
 		}
 		prec += 3;
 	}
-/*	if (pow >= 0)
-	{
-		
-	}*/
+	/*	if (pow >= 0)
+		{
+
+		}*/
 	prec += sign == -1 || strchr(flags, ' ') || strchr(flags, '+') ? 1 : 0;
 	return (prec);
 }
 
+int		ft_exp(int n, int exp)
+{
+	int		ret;
+
+	ret = 1;
+	while (exp--)
+		ret *= 10;
+	return (ret);
+}
+double	ft_exp_double(int n, int exp)
+{
+	int		ret;
+
+	ret = 1;
+	while (exp--)
+		ret *= n;
+	return (ret);
+}
+
+double	ft_round_up(double nb, int pow, int prec, int i)
+{
+	printf("%d\n", (ft_exp(10, pow) * 9));
+	if ((int)nb >= (ft_exp(10, pow) * 9) && i > prec)
+	{
+		i++;
+		ft_round_up((int)nb / 10, pow - 1, prec, i);
+	}
+	else if (i == prec)
+		nb = ft_exp_double(10, pow + 1);
+	printf("%f\n", nb);
+	printf("%d\n", i);
+	return (nb);
+}
+/*
+double	ft_round_up(double nb, int pow, int prec)
+{
+	int		i;
+
+	i = -1;
+	if (pow >= 0)
+		while (++i <= prec)
+			if ((int)(nb / ft_exp(10, prec)) % ft_exp(10, pow - (i + 1)) != 9)
+				break;
+	printf("i == %d\n", i);
+	printf("prec == %d\n", prec);
+	printf("pow == %d\n", pow);
+	if (i == prec + 1)
+		if ((int)nb % ft_exp(10, pow - prec - 1) == 9)
+			nb = ft_exp_double(10, pow + 1);
+	printf("oui ==%f\n", nb);
+	return (nb);
+}
+*/
 int		ft_format_size_g(double nb, char *flags)
 {
 	int		ret;
 	int		pow;
 	int		sign;
+	int		prec;
+	int		i;
 
+	i = 0;
+	prec = strchr(flags, '.') ? atoi(strchr(flags, '.') + 1) : 6;
+	pow = ft_get_pow(nb);
+	nb = ft_round_up(nb, pow, prec, i);
 	sign = nb < 0.0 ? -1 : 1;
 	nb *= sign;
-	pow = ft_get_pow(nb);
 	printf("pow == %d\n", pow);
 	if (!strchr(flags, '.') || pow < 0)
 	{
@@ -209,44 +238,48 @@ int		ft_format_size_g(double nb, char *flags)
 
 void delay(int number_of_seconds) 
 { 
-    // Converting time into milli_seconds 
-    int milli_seconds = 1000 * number_of_seconds; 
-  
-    // Storing start time 
-    clock_t start_time = clock(); 
-  
-    // looping till required time is not achieved 
-    while (clock() < start_time + milli_seconds) 
-        ; 
+	// Converting time into milli_seconds 
+	int milli_seconds = 1000 * number_of_seconds; 
+
+	// Storing start time 
+	clock_t start_time = clock(); 
+
+	// looping till required time is not achieved 
+	while (clock() < start_time + milli_seconds) 
+		; 
 }
 
 int main()
 {
 	int i;
-	double j = 9090000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.00;
+	double j = 9990000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.00;
 	double f = 0.000099;
-//	while (1)
-//	{
-/*	i = ft_format_size_g(j, " .g");
-	printf("%d\n", i);
-	i = printf("% .2g", j);
-	printf("\n%d\n", i);*/
+	double k = 99999000.00;
+	//	while (1)
+	//	{
+	/*	i = ft_format_size_g(j, " .g");
+		printf("%d\n", i);
+		i = printf("% .2g", j);
+		printf("\n%d\n", i);*/
 	printf("====E====\n");
-	i = ft_format_size_g(f, " .g");
+	i = ft_format_size_g(k, " .2g");
 	printf("%d\n", i);
-	i = printf("% .2g", f);
+	i = printf("% .4g", k);
 	printf("\n%d\n", i);
-	printf("====F====\n");
-	i = ft_format_size_g(0.0000999, " .g");
-	printf("%d\n", i);
-	i = printf("% .2g", 0.0000999);
-	printf("\n%d\n", i);
-//	delay(2);
-//	sleep(1000);
-//	}
+	/*	printf("====F====\n");
+		i = ft_format_size_g(0.0000999, " .g");
+		printf("%d\n", i);
+		i = printf("% .3g", 0.0000999);
+		printf("\n%d\n", i);*/
+	//	delay(2);
+	//	sleep(1000);
+	//	}
 }
-
-
+/*if 10 % 10 exposant pow - precision == 9, arrondir, que des 9?
+  si oui nb = 10 exposant pow + 1
+  besoin  de functions exposant
+  voir comment faire pour exposant negatif
+  */
 // probleme pour exp negatif sur g_f
 // probleme # sur g_f
 // g_e normalement good
