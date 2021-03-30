069b2a4ac59224520657177bcef798c241baf615
Ernest Dassesse <dassesse.ernestoutlook.com>
2021-03-30 13:16:44 +0200

    oui

--- srcs/ft_format_functions/ft_format_f.c
+++ srcs/ft_format_functions/ft_format_f.c
@@ -6,7 +6,7 @@
 /*   By: edassess <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 13:30:13 by edassess          #+#    #+#             */
-/*   Updated: 2021/02/02 13:30:49 by edassess         ###   ########lyon.fr   */
+/*   Updated: 2021/03/30 13:15:10 by edassess         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,6 @@ int		ft_format_f(t_printf data, t_flags flag, va_list ap)
 {
 	t_dbl	v;
 	int	size;
-
-	//	get size
 	v = ft_getdbl(ft_get_ap_f(ap, data.s));
 	if (v.pow >= 0)
 	{
@@ -38,7 +36,6 @@ int		ft_format_f(t_printf data, t_flags flag, va_list ap)
 			else
 				size = 1;
 		}
-		flag.min = 0;
 		size += v.pow;
 		if (flag.prec > -1)
 			size += flag.prec;
@@ -50,28 +47,19 @@ int		ft_format_f(t_printf data, t_flags flag, va_list ap)
 		if (ft_search(data.s, " ") || ft_search(data.s, "+") || v.sign < 0)
 			size++;
 		flag.zero -= size;
-
-		//	flag nbr
 		if (flag.nbr > size)
 			data.ret += ft_flag_number(flag, size);
-
-		//	space < plus flag
 		if (v.sign < 0)
 			ft_putchar_fd('-', 1);
 		else if (ft_search(data.s, "+"))
 			ft_flag_plus();
 		else if (ft_search(data.s, " "))
 			ft_flag_spc();
-
-		//	zero flag
 		if (flag.zero > 0)
 			ft_flag_zero(flag);
 		v.pow++;
 		while (v.pow--)
-		{
 			ft_putchar_fd(*v.decimal++, 1);
-			printf("1");
-		}
 		if (ft_search(data.s, "#") || flag.prec > 0)
 			ft_putchar_fd('.', 1);
 		while (flag.prec--)
@@ -81,11 +69,53 @@ int		ft_format_f(t_printf data, t_flags flag, va_list ap)
 			else
 				ft_putchar_fd('0', 1);
 		}
+		while (size++ < flag.min)
+			ft_putchar_fd(' ', 1);
 	}
 	else
 	{
-	size = 0;
+		size = 2;
+		if (flag.prec == 0)
+		{
+			if (ft_search(data.s, "#"))
+				size = 2;
+			else
+				size = 1;
+		}
+		if (flag.prec > -1)
+			size += flag.prec;
+		if (flag.prec == -1)
+			size += 6;
+		flag.zero -= size;
+		if (flag.nbr > size)
+			data.ret += ft_flag_number(flag, size);
+		if (v.sign < 0)
+			ft_putchar_fd('-', 1);
+		else if (ft_search(data.s, "+"))
+			ft_flag_plus();
+		else if (ft_search(data.s, " "))
+			ft_flag_spc();
+		if (flag.zero > 0)
+			ft_flag_zero(flag);
+		ft_putchar_fd('0', 1);
+		size--;
+		v.pow++;
+		if (flag.prec == 0 && ft_search(data.s, "#"))
+			ft_putchar_fd('.', 1);
+		if (flag.prec != 0)
+		{
+			ft_putchar_fd('.', 1);
+			size--;
+			while (v.pow++ < 0 && size--)
+				ft_putchar_fd('0', 1);
+			while (--size)
+			{
+				if (*v.decimal)
+					ft_putchar_fd(*v.decimal++, 1);
+				else
+					ft_putchar_fd('0', 1);
+			}
+		}
 	}
-	//	ft_putstr_fd(v.decimal, 1);
 	return (data.ret + size);
 }
--- test.c
+++ test.c
@@ -3,10 +3,10 @@
 
 int	main(void)
 {
-	ft_printf("coucou nous==%f\n", -0.00032);
-	printf("coucou real==%f\n", -0.00032);
-	ft_printf("coucou nous==%f\n", 90990.120);
-	printf("coucou real==%f\n", 90990.120);
+	ft_printf("coucou nous==%.3f\n", 1.2365);
+	printf("coucou real==%.3f\n", 1.2365);
+	ft_printf("coucou nous==%#12.0f\n", 0.0003);
+	printf("coucou real==%#12.0f\n", 0.0003);
 	return (0);
 
 
@@ -14,5 +14,8 @@ int	main(void)
 /*
 1.5 affiche 0.5
 90990.120 devient 90990.119
+arrondi
+des chiffres qui partent en couille
+problemes de conversion i think
 */
 }
