5b21aa9eebda1072afdef5afb747b750e1736df6
CanardWc <canardwc@MacBook-Pro-de-CanardWc.local>
2021-05-12 12:27:39 +0200

    adding inf gestionn

--- srcs/ft_format_functions/ft_format_f.c
+++ srcs/ft_format_functions/ft_format_f.c
@@ -12,6 +12,8 @@ int	ft_format_f(t_printf data, t_flags flags, va_list ap)
 	if (v.pow >= 400)
 	{
 		size = ft_strlen(limits[v.pow - 400]);
+		if (v.pow - 400 == 1 && (ft_search(data.s, "+") || ft_search(data.s, " ")))
+			size++;
 		if (flags.nbr > size)
 			data.ret += ft_flag_number(flags, size);
 		if (flags.zero > size)
@@ -20,6 +22,10 @@ int	ft_format_f(t_printf data, t_flags flags, va_list ap)
 			while (flags.zero-- > size)
 				ft_putchar_fd(' ', 1);
 		}
+		if (v.pow - 400 == 1 && ft_search(data.s, "+"))
+			ft_putchar_fd('+', 1);
+		else if (v.pow - 400 == 1 && ft_search(data.s, " "))
+			ft_putchar_fd(' ', 1);
 		ft_putstr_fd(limits[v.pow - 400], 1);
 		if (flags.min > size)
 			data.ret += ft_flag_min(flags, size);
